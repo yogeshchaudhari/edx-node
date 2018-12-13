f341bac83d536a7c35df175833ad2b7b03794b98
yogesh <yogesh.chaudhari@yaaman.com>
2018-12-13 12:32:07 +0530

    Created http server

---
+++ 07httpServer.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const port = process.argv[2] || 3000;
+
+http.createServer((request, response) => {
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.end('Hello world\n');
+}).listen(port);
+
+console.log(`Server running on port ${port}`);
